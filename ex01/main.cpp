a96d12bcae8a70bb4aa421e7a2e6d06977f98e53
oshiro17 <nonokapiano@icloud.com>
2023-08-26 22:10:57 +0900

    [fix]enterを押したときの処理

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,32 +7,21 @@ int main()
 	while (1)
 	{
 		std::cout << "Enter a Command : ";
-		if(!(std::getline(std::cin, ia)))
-		{
+		if (!(std::getline(std::cin, ia)))
 			exit(1);
-			if (std::cin.eof())
-			{
-				std::cout << "Exiting the program." << std::endl;
-				break;
-			}
-			else
-			{
-				std::cin.clear();
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				continue;
-			}
-		}
-		std::cout << std::endl;
-		if (ia == "SEARCH\0")
-			book.search();
-		else if(ia == "ADD\0")
+		if (std::cin.eof())
+			exit(1);
+		if (ia.compare("ADD") == 0)
 			book.add();
-		else if(ia == "EXIT\0")
+		else if (ia.compare("SEARCH") ==0 )
+			book.search();
+		else if(ia.compare("EXIT") == 0)
 		{
 			std::cout << "good bye( ≖ଳ≖)ㄑƺねႫ~" << std::endl;
 			break;
 		}
-		// std::cin.clear();
+		else
+			std::cout<<"wrong input(ꐦ°᷄д°᷅)\n";
 	}
 	return(0);
 }
