bbcef8c875f3fe68432d54411d3ad9514f80fffd
oshiro17 <nonokapiano@icloud.com>
2023-09-16 22:43:25 +0900

    dekita

---
+++ CPP01/ex03/HumanB.cpp
@@ -0,0 +1,24 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name)
+{
+    this->_name = name;
+}
+
+HumanB::~HumanB()
+{
+}
+
+void HumanB::setWeapon(Weapon& weapon)
+{
+    this->_weapon = &weapon;
+}
+
+void HumanB::attack(void)
+{
+	if (this->_weapon == NULL || this->_weapon->getType() == "")
+		std::cout << this->_name << " doesn't have a weapon to attack." << std::endl;
+	else
+		std::cout << this->_name << " attacks with their " <<  this->_weapon->getType() << std::endl;
+
+}
