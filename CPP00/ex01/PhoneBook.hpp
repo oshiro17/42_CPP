683fdd3e04e7ecc67f2614726fe4d8504907a921
oshiro17 <nonokapiano@icloud.com>
2023-10-04 15:56:17 +0900

    add files

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -3,27 +3,8 @@
 # include <iostream>
 # include <string>
 # include <sstream> 
+#include "Contact.hpp"
 
-class	Contact
-{
-	private:
-	std::string	_firstName;
-	int			_index;
-	std::string	_lastName;
-	std::string	_nickname;
-	std::string	_phoneNumber;
-	std::string	_darkestSecret;
-	std::string _inputContact(std::string str);
-	void	_putTenStr(size_t t_len,std::string str);
-
-	public:
-	void setContact(int index);//addの時
-	void putContact();
-	void putContactInfo();
-	Contact();
-	~Contact();
-	// init_con(void);
-};
 class	PhoneBook
 {
 	private:
---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+
+
+class Fixed
+{
+private:
+	int value;
+	const static int bits = 8;
+public:
+	int		getRawBits(void) const;
+	void	setRawBits( int const raw );
+	Fixed();
+	Fixed(int n)
+	~Fixed();
+	Fixed<< operator=(const Fixed& obj);
+	Fixed& operator=(const Fixed& obj);
+	Fixed(const Fixed& obj);
+};
+
+#endif
