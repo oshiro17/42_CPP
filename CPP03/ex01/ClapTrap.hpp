b7880898c3d22e2333cb39326d6cdebbb28382e0
oshiro17 <nonokapiano@icloud.com>
2023-10-17 18:00:31 +0900

    多分行けた

--- CPP03/ex01/ClapTrap.hpp
+++ CPP03/ex01/ClapTrap.hpp
@@ -4,13 +4,13 @@
 
 class	ClapTrap
 {
-private:
+protected:
 	std::string		_name;
 	unsigned int	_hitPoint;
 	unsigned int	_energyPoint;
 	unsigned int	_attackDamage;
 public:
-	ClapTrap(std::string name);
+	ClapTrap(std::string name = "NO_NAME_HOGE");
 	ClapTrap(const ClapTrap& obj);
 	~ClapTrap();
 	void attack(const std::string& target);
@@ -19,4 +19,16 @@ public:
 	ClapTrap& operator=(const ClapTrap& obj);
 	void showStatus();
 };
-#endif;
+class ScavTrap : public ClapTrap
+{
+public:
+	ScavTrap(std::string name);
+	ScavTrap(const ScavTrap& obj);
+	ScavTrap& operator=(const ScavTrap& obj);
+	~ScavTrap();
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+	void guardGate();
+};
+#endif
